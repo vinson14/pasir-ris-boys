8ef1a12d4fceb4499913c71b3648ab8e130b3755
Vinson Ong <vinson1493@gmail.com>
2021-11-21 00:27:53 +0800

    First prototype completed without bugs

--- utils/api.js
+++ utils/api.js
@@ -35,3 +35,82 @@ export const submitRecord = async (record) => {
     return false;
   }
 };
+
+// const oneTimeAdd = async () => {
+//   const records = [
+//     {
+//       date: "9 Jan 2021",
+//       vinson: 45,
+//       chimin: -45,
+//       junhui: 0,
+//     },
+//     {
+//       date: "24 Jan 2021",
+//       vinson: 275,
+//       chimin: 50,
+//       junhui: -325,
+//     },
+//     {
+//       date: "12 Feb 2021",
+//       vinson: 130,
+//       chimin: 15,
+//       junhui: -145,
+//     },
+//     {
+//       date: "7 Mar 2021",
+//       vinson: 115,
+//       chimin: 135,
+//       junhui: -250,
+//     },
+//     {
+//       date: "17 Apr 2021",
+//       vinson: 35,
+//       chimin: -110,
+//       junhui: 75,
+//     },
+//     {
+//       date: "1 May 2021",
+//       vinson: 145,
+//       chimin: -135,
+//       junhui: -10,
+//     },
+//     {
+//       date: "15 May 2021",
+//       vinson: 105,
+//       chimin: -55,
+//       junhui: -50,
+//     },
+//     {
+//       date: "30 May 2021",
+//       vinson: 200,
+//       chimin: -350,
+//       junhui: 150,
+//     },
+//     {
+//       date: "12 Jun 2021",
+//       vinson: 300,
+//       chimin: -200,
+//       junhui: -100,
+//     },
+//     {
+//       date: "10 Jul 2021",
+//       vinson: 0,
+//       chimin: -190,
+//       junhui: 190,
+//     },
+//     {
+//       date: "14 Aug 2021",
+//       vinson: 0,
+//       chimin: -200,
+//       junhui: 200,
+//     },
+//   ];
+
+//   records.forEach((record) => {
+//     const init = {
+//       headers: {},
+//       body: record,
+//     };
+//     const response = API.post(apiName, path, init);
+//   });
+// };
