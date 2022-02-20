c36342c05d1d9a35b818a09a8f03860645ac179e
Vinson Ong <vinson1493@gmail.com>
2022-02-20 17:10:59 +0800

    [FEATURE] Implemented filter by year in view records

--- components/view-records/view-records-modals.js
+++ components/view-records/view-records-modals.js
@@ -1,13 +1,40 @@
-import { Grid } from "@mui/material";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Grid,
+} from "@mui/material";
 import ModalContainer from "../stateless/misc/modal-container";
 import Record from "./record";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import _ from "lodash";
 
 const ViewRecordsModal = ({ open, onClose, records }) => {
+  const years = _.range(2021, new Date().getFullYear() + 1);
   return (
     <ModalContainer open={open} onClose={onClose} title="Records">
-      <Grid container>
-        {records.map((record) => (
-          <Record record={record} key={record.dateCreated} onClose={onClose} />
+      <Grid container minWidth={500}>
+        {years.map((year) => (
+          <Grid item xs={12}>
+            <Accordion>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                {year}
+              </AccordionSummary>
+              <AccordionDetails>
+                {records
+                  .filter((record) => {
+                    return new Date(record.dateCreated).getFullYear() === year;
+                  })
+                  .map((record) => (
+                    <Record
+                      record={record}
+                      key={record.dateCreated}
+                      onClose={onClose}
+                    />
+                  ))}
+              </AccordionDetails>
+            </Accordion>
+          </Grid>
         ))}
       </Grid>
     </ModalContainer>
